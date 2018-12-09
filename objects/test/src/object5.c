97b81e5708c2f7511f200225dc2650de8285a268
Joël Gähwiler <joel.gaehwiler@gmail.com>
2018-12-09 13:09:40 +0100

    rearranged code

--- objects/test/src/object5.c
+++ objects/test/src/object5.c
@@ -12,11 +12,12 @@
 #include "neo3.h"
 
 #define NUM_PIXELS 11
+#define SUN_CHANGE 5000
 
 static uint8_t servoPos = 90;
 static uint8_t servoMax = 160;
 static uint8_t servoMin = 20;
-static uint32_t activeSun = 1;
+static uint32_t active_sun = 1;
 static uint32_t random = 1;
 static unsigned long last_change = 0;
 static uint8_t tower_R = 0;
@@ -38,58 +39,56 @@ void object5_setup() {
   neo5_show();
 
   // configure joystick
+  // TODO: Properly configure joystick.
   ESP_ERROR_CHECK(gpio_set_direction(GPIO_NUM_32, GPIO_MODE_INPUT));
   ESP_ERROR_CHECK(gpio_set_direction(GPIO_NUM_33, GPIO_MODE_INPUT));
   ESP_ERROR_CHECK(gpio_set_pull_mode(GPIO_NUM_32, GPIO_PULLUP_ONLY));
   ESP_ERROR_CHECK(gpio_set_pull_mode(GPIO_NUM_33, GPIO_PULLUP_ONLY));
 
-  // prepare in master config for h Bridge
+  // enable h-bridge
   gpio_config_t master = {.pin_bit_mask = GPIO_SEL_16,
       .mode = GPIO_MODE_OUTPUT,
       .pull_up_en = GPIO_PULLUP_ENABLE,
       .pull_down_en = GPIO_PULLDOWN_DISABLE,
       .intr_type = GPIO_INTR_DISABLE};
-
-  // configure in master pin
   ESP_ERROR_CHECK(gpio_config(&master));
   ESP_ERROR_CHECK(gpio_set_level(GPIO_NUM_16, 1));
 
-  // laser pins setup
-  gpio_config_t laser1 = {.pin_bit_mask = GPIO_SEL_19,
+  // configure sun 1
+  gpio_config_t sun1 = {.pin_bit_mask = GPIO_SEL_18,
       .mode = GPIO_MODE_OUTPUT,
       .pull_up_en = GPIO_PULLUP_ENABLE,
       .pull_down_en = GPIO_PULLDOWN_DISABLE,
       .intr_type = GPIO_INTR_DISABLE};
-
-  ESP_ERROR_CHECK(gpio_config(&laser1));
-  ESP_ERROR_CHECK(gpio_set_level(GPIO_NUM_19, 0));
-
-  gpio_config_t laser2 = {.pin_bit_mask = GPIO_SEL_18,
-      .mode = GPIO_MODE_OUTPUT,
-      .pull_up_en = GPIO_PULLUP_ENABLE,
-      .pull_down_en = GPIO_PULLDOWN_DISABLE,
-      .intr_type = GPIO_INTR_DISABLE};
-
-  ESP_ERROR_CHECK(gpio_config(&laser2));
+  ESP_ERROR_CHECK(gpio_config(&sun1));
   ESP_ERROR_CHECK(gpio_set_level(GPIO_NUM_18, 0));
 
-  gpio_config_t laser3 = {.pin_bit_mask = GPIO_SEL_23,
+  // configure sun 2
+  gpio_config_t sun2 = {.pin_bit_mask = GPIO_SEL_19,
       .mode = GPIO_MODE_OUTPUT,
       .pull_up_en = GPIO_PULLUP_ENABLE,
       .pull_down_en = GPIO_PULLDOWN_DISABLE,
       .intr_type = GPIO_INTR_DISABLE};
+  ESP_ERROR_CHECK(gpio_config(&sun2));
+  ESP_ERROR_CHECK(gpio_set_level(GPIO_NUM_19, 0));
 
-  ESP_ERROR_CHECK(gpio_config(&laser3));
-  ESP_ERROR_CHECK(gpio_set_level(GPIO_NUM_23, 0));
-
-  gpio_config_t laser4 = {.pin_bit_mask = GPIO_SEL_17,
+  // configure sun 3
+  gpio_config_t sun3 = {.pin_bit_mask = GPIO_SEL_17,
       .mode = GPIO_MODE_OUTPUT,
       .pull_up_en = GPIO_PULLUP_ENABLE,
       .pull_down_en = GPIO_PULLDOWN_DISABLE,
       .intr_type = GPIO_INTR_DISABLE};
-
-  ESP_ERROR_CHECK(gpio_config(&laser4));
+  ESP_ERROR_CHECK(gpio_config(&sun3));
   ESP_ERROR_CHECK(gpio_set_level(GPIO_NUM_17, 0));
+
+  // configure sun 4
+  gpio_config_t sun4 = {.pin_bit_mask = GPIO_SEL_23,
+      .mode = GPIO_MODE_OUTPUT,
+      .pull_up_en = GPIO_PULLUP_ENABLE,
+      .pull_down_en = GPIO_PULLDOWN_DISABLE,
+      .intr_type = GPIO_INTR_DISABLE};
+  ESP_ERROR_CHECK(gpio_config(&sun4));
+  ESP_ERROR_CHECK(gpio_set_level(GPIO_NUM_23, 0));
 }
 
 double object5_loop() {
@@ -126,12 +125,12 @@ double object5_loop() {
   // joystick
   if (gpio_get_level(GPIO_NUM_32) == 1 && servoPos < servoMax) { //left TODO  check pin number
     servoPos++;
-    servo_write1(servoPos);
+    servo_write1(180 - servoPos);
   }
 
   if (gpio_get_level(GPIO_NUM_33) == 1 && servoPos > servoMin) {//right TODO  check pin number
     servoPos--;
-    servo_write1(servoPos);
+    servo_write1(180 - servoPos);
   }
 
   if (servoPos > servoMax) {
@@ -143,49 +142,42 @@ double object5_loop() {
   // get current time
   unsigned long now = naos_millis();
 
-  if (last_change == 0 || now - last_change >= 20000) {
+  if (last_change == 0 || now - last_change >= SUN_CHANGE) {
     // save time
     last_change = now;
 
-    // randomly select sun:
-    while (activeSun == random) {
-      random = abs(esp_random() >> 30); // get just two bits from esp_random();
-      random += 1; // make sure we have values between 1 and 4
+//    // randomly select tower
+//    while (active_sun == random) {
+//      random = abs(esp_random() >> 30); // get just two bits from esp_random();
+//      random += 1; // make sure we have values between 1 and 4
+//    }
+//
+//    active_sun = random;
+
+    active_sun++;
+    if(active_sun > 4) {
+      active_sun = 1;
     }
 
-    activeSun = random;
-
     // laser/sun code
-    switch (activeSun) {
+    switch (active_sun) {
       case 1:
+        ESP_ERROR_CHECK(gpio_set_level(GPIO_NUM_18, 1));
         ESP_ERROR_CHECK(gpio_set_level(GPIO_NUM_19, 0));
-        ESP_ERROR_CHECK(gpio_set_level(GPIO_NUM_18, 0));
-        ESP_ERROR_CHECK(gpio_set_level(GPIO_NUM_23, 1));
         ESP_ERROR_CHECK(gpio_set_level(GPIO_NUM_17, 0));
-        neo5_set_one(0, 255, 200, 0);
+        ESP_ERROR_CHECK(gpio_set_level(GPIO_NUM_23, 0));
+        neo5_set_one(0, 0, 0, 0);
         neo5_set_one(1, 0, 0, 0);
         neo5_set_one(2, 0, 0, 0);
-        neo5_set_one(3, 0, 0, 0);
-        //   servoMin = 70; //todo find the servo min and max for all lasers
-        //    servoMax = 100;
+        neo5_set_one(3, 255, 200, 0);
+        //  servoMin = 90;
+        //   servoMax = 150;
         break;
       case 2:
-        ESP_ERROR_CHECK(gpio_set_level(GPIO_NUM_19, 0));
         ESP_ERROR_CHECK(gpio_set_level(GPIO_NUM_18, 0));
-        ESP_ERROR_CHECK(gpio_set_level(GPIO_NUM_23, 0));
-        ESP_ERROR_CHECK(gpio_set_level(GPIO_NUM_17, 1));
-        neo5_set_one(0, 0, 0, 0);
-        neo5_set_one(1, 255, 200, 0);
-        neo5_set_one(2, 0, 0, 0);
-        neo5_set_one(3, 0, 0, 0);
-        //     servoMin = 90;
-        //   servoMax = 160;
-        break;
-      case 3:
         ESP_ERROR_CHECK(gpio_set_level(GPIO_NUM_19, 1));
-        ESP_ERROR_CHECK(gpio_set_level(GPIO_NUM_18, 0));
-        ESP_ERROR_CHECK(gpio_set_level(GPIO_NUM_23, 0));
         ESP_ERROR_CHECK(gpio_set_level(GPIO_NUM_17, 0));
+        ESP_ERROR_CHECK(gpio_set_level(GPIO_NUM_23, 0));
         neo5_set_one(0, 0, 0, 0);
         neo5_set_one(1, 0, 0, 0);
         neo5_set_one(2, 255, 200, 0);
@@ -193,17 +185,29 @@ double object5_loop() {
         //    servoMin = 10;
         //    servoMax = 90;
         break;
+      case 3:
+        ESP_ERROR_CHECK(gpio_set_level(GPIO_NUM_19, 0));
+        ESP_ERROR_CHECK(gpio_set_level(GPIO_NUM_18, 0));
+        ESP_ERROR_CHECK(gpio_set_level(GPIO_NUM_17, 1));
+        ESP_ERROR_CHECK(gpio_set_level(GPIO_NUM_23, 0));
+        neo5_set_one(0, 0, 0, 0);
+        neo5_set_one(1, 255, 200, 0);
+        neo5_set_one(2, 0, 0, 0);
+        neo5_set_one(3, 0, 0, 0);
+        //     servoMin = 90;
+        //   servoMax = 160;
+        break;
       case 4:
         ESP_ERROR_CHECK(gpio_set_level(GPIO_NUM_19, 0));
-        ESP_ERROR_CHECK(gpio_set_level(GPIO_NUM_18, 1));
-        ESP_ERROR_CHECK(gpio_set_level(GPIO_NUM_23, 0));
+        ESP_ERROR_CHECK(gpio_set_level(GPIO_NUM_18, 0));
         ESP_ERROR_CHECK(gpio_set_level(GPIO_NUM_17, 0));
-        neo5_set_one(0, 0, 0, 0);
+        ESP_ERROR_CHECK(gpio_set_level(GPIO_NUM_23, 1));
+        neo5_set_one(0, 255, 200, 0);
         neo5_set_one(1, 0, 0, 0);
         neo5_set_one(2, 0, 0, 0);
-        neo5_set_one(3, 255, 200, 0);
-        //  servoMin = 90;
-        //   servoMax = 150;
+        neo5_set_one(3, 0, 0, 0);
+        //   servoMin = 70; //todo find the servo min and max for all lasers
+        //    servoMax = 100;
         break;
       default:
         break;
