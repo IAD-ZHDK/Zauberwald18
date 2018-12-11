988d5a9e9b113c92d6b91194c71291e60807ad72
Joël Gähwiler <joel.gaehwiler@gmail.com>
2018-12-11 12:12:56 +0100

    make sure sketch exists on a crash

--- visualization/src/Sketch.java
+++ visualization/src/Sketch.java
@@ -26,7 +26,7 @@ public class Sketch extends PApplet {
   private wawiso.Visualization viz6;
 
   public void settings() {
-    if(DEBUG) {
+    if (DEBUG) {
       size(1200, 800, P3D);
     } else {
       fullScreen(P3D);
@@ -34,116 +34,132 @@ public class Sketch extends PApplet {
   }
 
   public void setup() {
-    // create mqtt client
-    client = new MQTTClient(this);
-    client.connect("mqtt://0.0.0.0:1884", "visualization");
+    try {
+      // create mqtt client
+      client = new MQTTClient(this);
+      client.connect("mqtt://0.0.0.0:1884", "visualization");
 
-    // subscribe to topics
-    client.subscribe("#");
+      // subscribe to topics
+      client.subscribe("#");
 
-    // load mask
-    mask = loadShape("mask.svg");
-    mask.setFill(color(0, 0, 0));
-    mask.scale(height / mask.height);
-    mask.translate((mask.width * (height / mask.height) - width) / -2, 0);
+      // load mask
+      mask = loadShape("mask.svg");
+      mask.setFill(color(0, 0, 0));
+      mask.scale(height / mask.height);
+      mask.translate((mask.width * (height / mask.height) - width) / -2, 0);
 
-    // create visualizations
-    viz1 = new kenos.Visualization(this);
-    viz2 = new electroswing.Visualization(this);
-    viz3 = new dedo.Visualization(this);
-    viz4 = new Visualization(this);
-    viz5 = new radar.Visualization(this);
-    viz6 = new wawiso.Visualization(this);
+      // create visualizations
+      viz1 = new kenos.Visualization(this);
+      viz2 = new electroswing.Visualization(this);
+      viz3 = new dedo.Visualization(this);
+      viz4 = new Visualization(this);
+      viz5 = new radar.Visualization(this);
+      viz6 = new wawiso.Visualization(this);
 
-    // setup visualization
-    viz1.setup();
-    viz2.setup();
-    viz3.setup();
-    viz4.setup();
-    viz5.setup();
-    viz6.setup();
+      // setup visualization
+      viz1.setup();
+      viz2.setup();
+      viz3.setup();
+      viz4.setup();
+      viz5.setup();
+      viz6.setup();
 
-    // set start
-    start = millis();
+      // set start
+      start = millis();
 
-    // set first visualization
-    current = 1;
+      // set first visualization
+      current = 1;
+    } catch (Exception e) {
+      e.printStackTrace();
+      exit();
+    }
   }
 
   public void draw() {
-    // get time difference
-    int diff = millis() - start;
+    try {
+      // get time difference
+      int diff = millis() - start;
 
-    // get max input levels
-    float water = max(water1, water2);
-    float wind = max(wind1, wind2);
-    float solar = max(solar1, solar2);
+      // get max input levels
+      float water = max(water1, water2);
+      float wind = max(wind1, wind2);
+      float solar = max(solar1, solar2);
 
-    // push matrix and style
-    this.pushMatrix();
-    this.pushStyle();
+      // push matrix and style
+      this.pushMatrix();
+      this.pushStyle();
 
-    // draw visualization
-    switch (current) {
-      case 1:
-        viz1.draw(water, wind, solar);
-        break;
-      case 2:
-        viz2.draw(water, wind, solar);
-        break;
-      case 3:
-        viz3.draw(water, wind, solar);
-        break;
-      case 4:
-        viz4.draw(water, wind, solar);
-        break;
-      case 5:
-        viz5.draw(water, wind, solar);
-        break;
-      case 6:
-        viz6.draw(water, wind, solar);
-        break;
-    }
-
-    // pop matrix and style
-    this.popStyle();
-    this.popMatrix();
-
-    // draw mask
-    fill(0, 0, 0);
-    noStroke();
-    shape(mask, 0, 0);
-    rect(0, 0, (mask.width * (height / mask.height) - width) / -2, height);
-    rect(((mask.width * (height / mask.height) - width) / -2) + mask.width * (height / mask.height), 0, width, height);
-
-    // calculate fade
-    float fade = 0;
-    if (diff < FADE_LENGTH) {
-      fade = constrain(map(diff, 0, FADE_LENGTH, 1, 0), 0, 1);
-    } else if (diff > (LENGTH - FADE_LENGTH)) {
-      fade = constrain(map(diff, (LENGTH - FADE_LENGTH), LENGTH, 0, 1), 0, 1);
-    }
-
-    // apply fade
-    if (fade > 0) {
-      fill(0, fade * 255);
-      rect(0, 0, width, height);
-    }
-
-    // reset time and select next visualization
-    if (diff >= LENGTH) {
-      start = millis();
-      current++;
-      if (current > 6) {
-        current = 1;
+      // draw visualization
+      switch (current) {
+        case 1:
+          viz1.draw(water, wind, solar);
+          break;
+        case 2:
+          viz2.draw(water, wind, solar);
+          break;
+        case 3:
+          viz3.draw(water, wind, solar);
+          break;
+        case 4:
+          viz4.draw(water, wind, solar);
+          break;
+        case 5:
+          viz5.draw(water, wind, solar);
+          break;
+        case 6:
+          viz6.draw(water, wind, solar);
+          break;
       }
-    }
 
-    // draw current viz and frame rate
-    if (DEBUG) {
-      fill(255);
+      // pop matrix and style
+      this.popStyle();
+      this.popMatrix();
+
+      // draw mask
+      fill(0, 0, 0);
       noStroke();
-      text(current + " - " + frameRate, 10, 20);
+      shape(mask, 0, 0);
+      rect(0, 0, (mask.width * (height / mask.height) - width) / -2, height);
+      rect(
+          ((mask.width * (height / mask.height) - width) / -2)
+              + mask.width * (height / mask.height),
+          0,
+          width,
+          height);
+
+      // calculate fade
+      float fade = 0;
+      if (diff < FADE_LENGTH) {
+        fade = constrain(map(diff, 0, FADE_LENGTH, 1, 0), 0, 1);
+      } else if (diff > (LENGTH - FADE_LENGTH)) {
+        fade = constrain(map(diff, (LENGTH - FADE_LENGTH), LENGTH, 0, 1), 0, 1);
+      }
+
+      // apply fade
+      if (fade > 0) {
+        fill(0, fade * 255);
+        rect(0, 0, width, height);
+      }
+
+      // reset time and select next visualization
+      if (diff >= LENGTH) {
+        start = millis();
+        current++;
+        if (current > 6) {
+          current = 1;
+        }
+      }
+
+      // draw current viz and frame rate
+      if (DEBUG) {
+        fill(255);
+        noStroke();
+        text(current + " - " + frameRate, 10, 20);
+      }
+
+    } catch (Exception e) {
+      e.printStackTrace();
+      exit();
     }
   }
 
